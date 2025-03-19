f4d810956b2a948f0577d9bb0096e8e63f3e4211
arodriguez1981 <arodriguez1981@gmail.com>
2025-03-19 15:42:49 -0400

    Initial Commit

---
+++ Sat Game Challenge/Sat_Game_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sat_Game_ChallengeApp.swift
+//  Sat Game Challenge
+//
+//  Created by Alex on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Sat_Game_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sat Game ChallengeTests/Sat_Game_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sat_Game_ChallengeTests.swift
+//  Sat Game ChallengeTests
+//
+//  Created by Alex on 3/19/25.
+//
+
+import Testing
+@testable import Sat_Game_Challenge
+
+struct Sat_Game_ChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
